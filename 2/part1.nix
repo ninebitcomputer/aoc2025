f200616db2436f5263f482accdb3fc2a2f1a638e
Daniel Jin <mrpickles123115@gmail.com>
2025-12-07 13:29:35 -0500

    correct invalid check

--- 2/part1.nix
+++ 2/part1.nix
@@ -9,14 +9,22 @@ let
   makeConstraint = p:
 	let
 	  upper = (pow 10 p);
-	  mod = (pow 10 (2 * p + 1));
-	in 3;
+	  mod = (pow 10 (2 * p + 1)) + upper;
+	in {
+	  inherit mod upper;
+	};
 
   constraints = genList makeConstraint 5;
+  fit = num : constraint :
+	let
+	  q = num / constraint.mod;
+	in
+	  (q < constraint.upper) && (num == q * constraint.mod);
   
-  #invalid = num: any (c: (num < c.upper) && (mod num c.mod) == 0) constraints;
+  #invalid = num: any (c: trace c ((num < c.upper) && (mod num c.mod) == 0)) constraints;
+  invalid = num: any (c: (fit num c)) constraints;
   #invalid = num: any (c: false) constraints;
-  invalid = num: false;
+  #invalid = num: false;
 
   g = r :
 	let
@@ -26,10 +34,10 @@ let
 		  (range r.beg (r.end + 1))
 		);
 	in
+	  trace filtered
 		(sumList filtered);
 
-  #solution = sumList (map g data.sorted);
-  solution = data.sorted;
+  solution = sumList (map g data.sorted);
 in {
   inherit solution;
 }
--- utils.nix
+++ utils.nix
@@ -7,7 +7,20 @@ let
 	  if (v < 0) then v + b else v;
 
   split = regex : input : builtins.filter (s : s != "" && !builtins.isList s) (builtins.split regex input);
+
   flatten = builtins.foldl' (acc : cur : acc ++ cur) [];
+  sumList = builtins.foldl' (a : b : a + b) 0;
+  range = a : b : builtins.genList (x: a + x) (b - a);
+
+  _pow = base: p: acc:
+	if p == 0 then
+	  acc
+	else if (builtins.bitAnd p 1) == 1 then
+	  _pow base (p - 1) (acc * base)
+	else
+	  _pow (base * base) (p / 2) acc;
+  pow = base : p : _pow base p 1;
+
 in {
-  inherit mod nmod split flatten;
+  inherit mod nmod split flatten pow sumList range;
 }
